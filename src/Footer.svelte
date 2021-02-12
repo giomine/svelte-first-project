a2b548dfca1a64f8216271cbd10fa45e9b12561f
giomine <giomineo@gmail.com>
2021-02-12 17:32:40 +0000

    Add temp logos to footer component

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,29 @@
 
 
 <main>
+    <div class="container">
+        <div class="icon-container">
+            <div class="icons">
+                <img src="/images/rating.png" alt="rating logo">
+            </div>
+            <div class="icons">
+                <img src="/images/xbox-logo.png" alt="Xbox logo">
+            </div>
+            <div class="icons">
+                <img src="/images/ps4logo.png" alt="playstation 4 logo">
+            </div>
+            <div class="icons">
+                <img src="/images/steamlogo.png" alt="steam logo">
+            </div>
+            <div class="icons">
+                <img src="/images/pclogo.png" alt="PC logo">
+            </div>   
+        </div>
 
+        <div class="squarelogo">
+            <img src="/images/squarelogo.png" alt="">
+        </div>
+    </div>
 </main>
 
 
@@ -14,5 +36,26 @@
     main{
 		height: 50vh;
 		background: white;
-	}
+    }
+    img{
+        width: 180px;
+    }
+    .icon-container{
+        height: 100px;
+        margin: 50px 200px;
+        display: flex;
+        justify-content: space-between;
+
+    }
+    .icons{
+        display: flex;
+        align-items: center;
+    }
+    .squarelogo{
+        display: flex;
+        justify-content: center;
+    }
+    .squarelogo img {
+        width: 250px;
+    }
 </style>
