3a9f745830612f1f556265877de432dbfacfff28
giomine <giomineo@gmail.com>
2021-02-12 15:24:08 +0000

    Play with button stylings

--- src/Button.svelte
+++ src/Button.svelte
@@ -28,11 +28,15 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        /* background-image: url("/images/buttonbg.jpg"); */
+        /* background: linear-gradient(0deg, rgba(187, 187, 187, 0.5), rgba(187, 187, 187, 0.5)), url("/images/buttonbg.jpg"); */
     }
     .button:hover{
     	transform: translate3D(0, -2px, 0);
     }
     .color{
         background: rgb(184, 62, 16);
+        /* background: linear-gradient(0deg, rgba(122, 40, 10, 0.7), rgba(122, 40, 10, 0.7)), url("/images/buttonbg.jpg"); */
+        
     }
 </style>
