aa4422f1fdc35c721dbb33e7cc48a3aecff58dd5
giomine <giomineo@gmail.com>
2021-02-12 17:51:23 +0000

    Add legal text

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -27,6 +27,17 @@
         <div class="squarelogo">
             <img src="/images/squarelogo.png" alt="">
         </div>
+
+        <div class="legal">
+            <p>Tomb Raider © 2018 Square Enix Ltd. Square Enix and the Square Enix logo are registered trademarks of Square Enix
+                Holdings Co., Ltd. Lara Croft, Tomb Raider, Crystal Dynamics, the Crystal Dynamics logo, Eidos, and the Eidos logo are 
+                trademarks of Square Enix Ltd. "Playstation" and the "PS" Family logo are registered trademarks and "PS3", "PS4" and the 
+                Playstation Network logo are trademarks of Sony Computer Entertainment Inc. Microsoft, Kinect, Xbox, Xbox 360, Xbox LIVE,
+                 and the Xbox logos are trademarks of The Microsoft Group of companies and are used under license from Microsoft. The rating 
+                 icon is a registered trademark of the Pan European Game Information. All other trademarks are the property of their respective 
+                 owners. All rights reserved.
+            </p>
+        </div>
     </div>
 </main>
 
@@ -57,5 +68,14 @@
     }
     .squarelogo img {
         width: 250px;
+    }
+
+    .legal{
+        font-size: 10.4px;
+        margin: 25px 30px;
+        line-height: 1.6;
+        letter-spacing: .01em;
+        text-align: center;
+        color: rgb(150, 150, 150);
     }
 </style>
